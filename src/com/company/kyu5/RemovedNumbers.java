4402fa90bad03b5518ef0b6fab842b8e8a58a79d
Maxim Chechetkin <max1.chechetkin@gmail.com>
2019-08-21 21:41:00 +0300

    Add solutions to kyu5

---
+++ src/com/company/kyu5/RemovedNumbers.java
@@ -0,0 +1,21 @@
+package com.company.kyu5;
+
+import java.util.*;
+
+/*
+    Condition: https://www.codewars.com/kata/5547cc7dcad755e480000004
+ */
+
+public class RemovedNumbers {
+    public static List<long[]> removNb(long n) {
+        long sum=n * (n + 1) / 2;
+        List<long[]> list=new ArrayList<>();
+        for (long i=1;i<=n;i++){
+            long j = (sum - i) / (i + 1);
+            if (j<n && i * j == sum - i - j) {
+                list.add(new long[] {i, j});
+            }
+        }
+        return list;
+    }
+}
