a734e909a5e690670b8bf69f4b35e220e5cda856
Maxim Chechetkin <max1.chechetkin@gmail.com>
2019-08-21 20:54:55 +0300

    Add solutions to kyu5

---
+++ src/com/company/kyu5/Line.java
@@ -0,0 +1,17 @@
+package com.company.kyu5;
+
+/*
+    Condition: https://www.codewars.com/kata/551dd1f424b7a4cdae0001f0
+ */
+
+public class Line {
+
+    public static String WhoIsNext(String[] names, int n) {
+        int numOfClons = 1;
+        while (names.length * numOfClons < n) {
+            n -= numOfClons * names.length;
+            numOfClons *= 2;
+        }
+        return names[(n-1)/numOfClons];
+    }
+}
---
+++ src/com/company/kyu5/TrailingZeros.java
@@ -0,0 +1,20 @@
+package com.company.kyu5;
+
+/*
+    Condition: https://www.codewars.com/kata/52f787eb172a8b4ae1000a34
+ */
+
+public class TrailingZeros {
+
+    public static int zeros(int n) {
+        return getSum(n, 1);
+    }
+
+    public static int getSum(int n, int pow) {
+        int nextNum = (int)Math.pow(5, pow);
+        if (nextNum > n) {
+            return 0;
+        }
+        return (n / nextNum) + getSum(n, ++pow);
+    }
+}
