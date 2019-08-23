4c846efff6ea42422b3e68a37776733323966cf1
Maxim Chechetkin <max1.chechetkin@gmail.com>
2019-08-23 22:48:15 +0300

    Add solutions to kyu5

---
+++ src/com/company/kyu5/Max.java
@@ -0,0 +1,20 @@
+package com.company.kyu5;
+
+/*
+    Condition: https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
+ */
+
+public class Max {
+
+    public static int sequence(int[] arr) {
+        int currSum = 0;
+        int maxSum = 0;
+
+        for(int element : arr) {
+            currSum = Math.max(0, currSum + element);
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+}
