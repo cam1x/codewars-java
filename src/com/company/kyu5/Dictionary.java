8cf41a13351af67ee30f0792a41a2bbfa208e075
Maxim Chechetkin <max1.chechetkin@gmail.com>
2019-08-26 12:38:21 +0300

    Add solutions to kyu5

---
+++ src/com/company/kyu5/Dictionary.java
@@ -0,0 +1,37 @@
+package com.company.kyu5;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+/*
+    Condition: https://www.codewars.com/kata/5259510fc76e59579e0009d4
+ */
+
+public class Dictionary {
+
+    private final String[] words;
+
+    public Dictionary(String[] words) {
+        this.words = words;
+    }
+
+    public String findMostSimilar(String to) {
+        return Arrays.stream(words)
+                .min(Comparator.comparingInt(str->getSimilarPoint(str,to)))
+                .get();
+    }
+
+    private int getSimilarPoint(String a, String b) {
+        return Math.max(a.length(), b.length()) - ((int) sameLettersLength(a, b));
+    }
+
+    private long sameLettersLength(String a, String b) {
+        if (a.length() > b.length()) {
+            return sameLettersLength(b, a);
+        }
+        return IntStream.rangeClosed(0, b.length() - a.length())
+                .mapToLong(i -> IntStream.range(0, a.length()).filter(j -> a.charAt(j) == b.charAt(i + j)).count())
+                .max()
+                .orElse(0);
+    }
+}
---
+++ src/com/company/kyu5/ToIPv4.java
@@ -0,0 +1,21 @@
+package com.company.kyu5;
+
+import java.util.stream.*;
+
+/*
+    Condition: https://www.codewars.com/kata/52e88b39ffb6ac53a400022e
+ */
+
+public class ToIPv4 {
+
+    public static String longToIP(long ip) {
+        StringBuilder sb=new StringBuilder(Long.toBinaryString(ip));
+        while (sb.length()<32){
+            sb.insert(0,0);
+        }
+        return Stream.of(sb.toString()
+                .split("(?=(.{8})+$)"))
+                .map(x->Integer.toString(Integer.parseInt(x,2)))
+                .collect(Collectors.joining("."));
+    }
+}
