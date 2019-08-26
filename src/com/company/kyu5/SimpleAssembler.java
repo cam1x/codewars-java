5f87212a0d0160c50e30fa9d98487072f7f719e7
Maxim Chechetkin <max1.chechetkin@gmail.com>
2019-08-26 20:49:33 +0300

    Add solutions to kyu5

---
+++ src/com/company/kyu5/SimpleAssembler.java
@@ -0,0 +1,45 @@
+package com.company.kyu5;
+
+import java.util.*;
+
+/*
+    Condition: https://www.codewars.com/kata/58e24788e24ddee28e000053
+ */
+
+public class SimpleAssembler {
+
+    public static Map<String, Integer> interpret(String[] program){
+        Map<String, Integer> registers=new HashMap<>();
+        for (int i=0;i<program.length;i++){
+            String[] instructions=program[i].split(" ");
+            switch (instructions[0]){
+                case "mov":{
+                    if (instructions[2].matches("\\-?\\d+")) {
+                        registers.put(instructions[1], Integer.parseInt(instructions[2]));
+                    }else{
+                        registers.put(instructions[1], registers.get(instructions[2]));
+                    }
+                    break;
+                }
+
+                case "inc":{
+                    registers.put(instructions[1],registers.get(instructions[1])+1);
+                    break;
+                }
+
+                case "dec":{
+                    registers.put(instructions[1],registers.get(instructions[1])-1);
+                    break;
+                }
+
+                case "jnz":{
+                    if (!registers.containsKey(instructions[1]) || registers.get(instructions[1])!=0){
+                        i+=Integer.parseInt(instructions[2])-1;
+                    }
+                    break;
+                }
+            }
+        }
+        return registers;
+    }
+}
