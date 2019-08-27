60bb1fd2bea8ba438699e0d4e7e18c3d0f532d50
Maxim Chechetkin <max1.chechetkin@gmail.com>
2019-08-27 20:04:04 +0300

    Add solutions to kyu5

---
+++ src/com/company/kyu5/Dec2Fact.java
@@ -0,0 +1,60 @@
+package com.company.kyu5;
+
+import java.util.*;
+
+/*
+    Condition: https://www.codewars.com/kata/54e320dcebe1e583250008fd
+ */
+
+public class Dec2Fact {
+
+    private static Map<Character, Integer> mapKeys = new HashMap<>();
+
+    static {
+        for (char i = '0'; i <= '9'; i++) {
+            mapKeys.put(i, Integer.parseInt(String.valueOf(i)));
+        }
+        for (char i = 'A'; i < 'Z'; i++) {
+            mapKeys.put(i, i - 55);
+        }
+    }
+
+    public static String dec2FactString(long nb) {
+        StringBuilder result = new StringBuilder();
+        Stack<Long> stack = new Stack<>();
+        long i = 0;
+        while (true) {
+            long base = factorial(i);
+            if (base > nb) {
+                break;
+            }
+            stack.push(base);
+            i++;
+        }
+        long temp = nb;
+        while (!stack.empty()) {
+            long multiply = temp / stack.peek();
+            temp -= multiply * stack.pop();
+            result.append(multiply>9?Character.toString((char)('A'+multiply-10)):multiply);
+        }
+        return result.toString();
+    }
+
+    public static long factString2Dec(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str).reverse();
+        long result=0, fact=1;
+        for (int i=1;i<stringBuilder.length();i++){
+            fact*=i;
+            result+=fact*mapKeys.get(stringBuilder.charAt(i));
+        }
+        return result;
+    }
+
+    private static long factorial(long n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
