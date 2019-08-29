9bcb298ef1c2c983251b1e2b4a7e758b635bd537
Maxim Chechetkin <max1.chechetkin@gmail.com>
2019-08-29 12:03:21 +0300

    Add solutions to kyu5

---
+++ src/com/company/kyu5/Dartboard.java
@@ -0,0 +1,47 @@
+package com.company.kyu5;
+
+/*
+    Condition: https://www.codewars.com/kata/5870db16056584eab0000006
+ */
+
+public class Dartboard {
+
+    private static final double bullEye=12.7;
+    private static final double bull=31.8;
+    private static final double tripleInner=198;
+    private static final double tripleOuter=214;
+    private static final double doubleInner=324;
+    private static final double doubleOuter=340;
+    private static final int[] sectors={20, 1, 18, 4, 13, 6, 10, 15,2, 17,
+                                        3, 19, 7, 16, 8, 11, 14, 9, 12, 5};
+
+    public static String getScore(double x, double y) {
+        if (x*x+y*y>=doubleOuter*doubleOuter/4){
+            return "X";
+        }
+        if (x*x+y*y<bullEye*bullEye/4){
+            return "DB";
+        }
+        if (x*x+y*y<bull*bull/4){
+            return "SB";
+        }
+        StringBuilder result=new StringBuilder();
+        if (x*x+y*y<tripleOuter*tripleOuter/4 && x*x+y*y>=tripleInner*tripleInner/4){
+            result.append("T");
+        }else{
+            if (x*x+y*y<doubleOuter*doubleOuter/4 && x*x+y*y>=doubleInner*doubleInner/4){
+                result.append("D");
+            }
+        }
+
+        int scIdx = (int)((getAngle(x, y)+9)/18);
+        scIdx = scIdx>=20?0:scIdx;
+
+        return result.append(sectors[scIdx]).toString();
+    }
+
+    private static double getAngle( double dx, double dy) {
+        double degree = (Math.atan2(dx, dy) * 180) / Math.PI;
+        return (degree<0)?degree+360:degree;
+    }
+}
---
+++ src/com/company/kyu5/MaximumContiguousSum.java
@@ -0,0 +1,18 @@
+package com.company.kyu5;
+
+/*
+    Condition: https://www.codewars.com/kata/52f4261c95d6bff39a003096
+ */
+
+public class MaximumContiguousSum {
+
+    public static int maxContiguousSum(final int[] arr) {
+        int currSum = 0;
+        int maxSum = 0;
+        for(int element : arr) {
+            currSum = Math.max(0, currSum + element);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+}
